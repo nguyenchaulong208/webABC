416461aca24e34ca9311e6bac302869582d9c17a
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-06-05 11:54:48 +0700

    Update list.js

--- db/list.js
+++ db/list.js
@@ -1,26 +1,54 @@
 const list = [{
     "id": 1,
-    "title": "Cherry Mỹ - hộp 500gram",
-    "price": 1.99,
+    "title": "CHERRY MỸ MORADA - HỘP 1KG",
+    "price": 390.000,
     "imgPath":"/images/cherry.jpg"
 },
 {
     "id": 2,
-    "title": "Táo túi core Nam Phi - túi 3kg",
-    "price": 1.99,
+    "title": "TÁO TÚI CORE NAM PHI NHẬP KHẨU - TÚI 1KG",
+    "price": 155.000,
     "imgPath":"/images/tao_core.jpg"
 },
 {
     "id": 3,
-    "title": "Quýt Úc - túi 800gram",
-    "price": 1.99,
-    "imgPath":"/images/quyt.jpg"
+    "title": "NHO NGÓN HELLO ÚC - 1KG",
+    "price": 135.000,
+    "imgPath":"/images/hello_uc.jpg"
 },
 {
     "id": 4,
-    "title": "Táo mật EverCrisp - túi 1kg",
-    "price": 1.99,
-    "imgPath":"/images/evercrisp.jpg"
+    "title": "NHO MẪU ĐƠN SHINE MUSCAT THÙNG HỒNG - 1 THÙNG",
+    "price": 300.000,
+    "imgPath":"/images/shine_muscat.jpg"
+}
+,
+{
+    "id": 5,
+    "title": "KIWI VÀNG ZESPRI NEWZELAND- HỘP 1KG",
+    "price":  155.000,
+    "imgPath":"/images/zespri_newzeland.jpg"
+}
+,
+{
+    "id": 6,
+    "title": "NHO MẪU ĐƠN RỔ HỒNG",
+    "price":  150.000,
+    "imgPath":"/images/maudon_ro.jpg"
+}
+,
+{
+    "id": 7,
+    "title": "DƯA QUEEN",
+    "price":  115.000,
+    "imgPath":"/images/queen.jpg"
+}
+,
+{
+    "id": 8,
+    "title": "YẾN SÀO NGUYÊN TỔ - BÌNH THUẬN - NHÀ NUÔI - 100GRAM",
+    "price":  3.400.000,
+    "imgPath":"/images/nest.jpg"
 }
 ]
 // chi so trang bat dau tu 1
